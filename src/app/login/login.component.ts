66b9b3ae09d71a307575de05400016d2a072626e
krzychu124 <krzychu124@gmail.com>
2018-04-22 23:35:44 +0200

    improved login and register layout and functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { AuthorizationService } from '@app/shared/service/authorization.service';
 import { Logincredentials } from '@app/shared/logincredentials';
 import { TokenRequestData } from '@app/shared/service/token-request-data';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,21 +13,36 @@ import { TokenRequestData } from '@app/shared/service/token-request-data';
 export class LoginComponent implements OnInit {
   password: FormControl = new FormControl('', Validators.required);
   userName: FormControl = new FormControl('', Validators.required);
+  loginForm: FormGroup;
   loginStatus: string;
-  constructor(private auth: AuthorizationService) { }
+  processing: boolean;
+  constructor(private auth: AuthorizationService, private router: Router) { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      password: this.password,
+      userName: this.userName
+    });
   }
 
   login() {
+    this.processing = true;
     this.auth.login({ userName: this.userName.value, password: this.password.value } as Logincredentials).subscribe(resp => {
-      this.loginStatus = 'ok';
+      this.processing = false;
+      setTimeout(() => {
+        this.router.navigate(['/']);
+      }, 500);
     }, err => {
       console.error(err);
-      this.loginStatus = 'failed';
+      if (err.status === 0) {
+        this.loginStatus = 'Błąd połączenia z serwerem...';
+      } else {
+        this.loginStatus = 'Nieprawidłowy login/hasło';
+      }
+      this.processing = false;
+      setTimeout(() => {
+        this.loginStatus = null;
+      }, 3000);
     });
-  }
-  logout() {
-    this.auth.logout().subscribe();
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '@app/shared/service/rest.service';
 import { User } from '@app/shared/user';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { AuthorizationService } from '@app/shared/service/authorization.service';
 import { Logincredentials } from '@app/shared/logincredentials';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +12,26 @@ import { Logincredentials } from '@app/shared/logincredentials';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  name: FormControl=new FormControl('', Validators.required);
-  surname: FormControl=new FormControl('', Validators.required);
-  email: FormControl=new FormControl('', Validators.required);
-  password: FormControl=new FormControl('', Validators.required);
-  userName: FormControl=new FormControl('', Validators.required);
-protected error: any;
-  constructor(private rest: RestService, private auth:AuthorizationService) { }
+  name: FormControl = new FormControl('', Validators.required);
+  surname: FormControl = new FormControl('', Validators.required);
+  email: FormControl = new FormControl('', Validators.required);
+  password: FormControl = new FormControl('', Validators.required);
+  userName: FormControl = new FormControl('', Validators.required);
+  registerForm: FormGroup;
+  processing: boolean;
+  protected error: any;
+  constructor(private rest: RestService, private auth: AuthorizationService, private router: Router) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      name: this.name,
+      surname: this.surname,
+      email: this.email,
+      password: this.password,
+      userName: this.userName
+    });
   }
-  register(){
+  register() {
     this.error = null;
     let user = new User();
     user.name = this.name.value;
@@ -29,14 +39,23 @@ protected error: any;
     user.email = this.email.value;
     user.userName = this.userName.value;
     user.password = this.password.value;
-    this.rest.registerUser(user).subscribe(resp=> {
-      console.info('Registered? ' + resp);
-    }, err=> {
-      this.error = 'Registration failed';
-      console.info('Registered? ' + err);
+    this.processing = true;
+    this.rest.registerUser(user).subscribe(resp => {
+      this.processing = false;
+      console.info('OK');
+      setTimeout(() => {
+        this.router.navigate(['/login']);
+      }, 500);
+    }, err => {
+      this.processing = false;
+      this.error = 'Wystąpił problem podczas rejestracji :(';
+      setTimeout(() => {
+        this.error = null;
+      }, 3000);
+      console.info(err);
     });
   }
-  login(){
-    this.auth.login({userName: "k", password: "p"} as Logincredentials).subscribe(resp => {console.log(resp);}, err => {console.error(err);});
+  login() {
+    this.auth.login({ userName: "k", password: "p" } as Logincredentials).subscribe(resp => { console.log(resp); }, err => { console.error(err); });
   }
 }
